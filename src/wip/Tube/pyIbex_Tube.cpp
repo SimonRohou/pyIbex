5812aad75ce340d18897b64ce9054d47b4be9b28
SimonRohou <simon.rohou@gmail.com>
2015-11-05 00:42:05 +0100

    completing binding for Tube

--- src/wip/Tube/pyIbex_Tube.cpp
+++ src/wip/Tube/pyIbex_Tube.cpp
@@ -16,7 +16,24 @@ using namespace ibex;
 
 void export_Tube() {
 
-    //typedef const ibex::Interval& (Tube::*getT_1) (int);
+    const Interval& (Tube::*getT_1)(void) = &Tube::getT;
+    const Interval& (Tube::*getT_2)(int) = &Tube::getT;
+    const Interval& (Tube::*getT_3)(double) = &Tube::getT;
+
+    const Interval& (Tube::*getY_1)(int) = &Tube::getY;
+    const Interval& (Tube::*getY_2)(double) = &Tube::getY;
+    Interval (Tube::*getY_3)(const Interval&) = &Tube::getY;
+
+    void (Tube::*setY_1)(const Interval&, int) = &Tube::setY;
+    void (Tube::*setY_2)(const Interval&, double) = &Tube::setY;
+    void (Tube::*setY_3)(const Interval&, const Interval&) = &Tube::setY;
+
+    bool (Tube::*intersect_1)(const Interval&, int) = &Tube::intersect;
+    bool (Tube::*intersect_2)(const Interval&, double) = &Tube::intersect;
+    bool (Tube::*intersect_3)(const Interval&, const Interval&, bool) = &Tube::intersect;
+
+    pair<Interval,Interval> (Tube::*getEnclosedBounds_1)(void) const = &Tube::getEnclosedBounds;
+    pair<Interval,Interval> (Tube::*getEnclosedBounds_2)(const Interval&) const = &Tube::getEnclosedBounds;
 
     class_<Tube, boost::shared_ptr<Tube> >("Tube", init<Interval,unsigned int>())
         .def(repr(self))
@@ -26,13 +43,22 @@ void export_Tube() {
         .def("getSlice", &Tube::getSlice, return_value_policy<manage_new_object>())
         .def("input2index", &Tube::input2index)
         .def("index2input", &Tube::index2input)
-        //.def("getT", getT_1(&Tube::getT))
-        //.def("getY", &Tube::getY)
-        //.def("setY", &Tube::setY)
+        .def("getT", getT_1, return_value_policy<copy_const_reference>())
+        .def("getT", getT_2, return_value_policy<copy_const_reference>())
+        .def("getT", getT_3, return_value_policy<copy_const_reference>())
+        .def("getY", getY_1, return_value_policy<copy_const_reference>())
+        .def("getY", getY_2, return_value_policy<copy_const_reference>())
+        .def("getY", getY_3)
+        .def("setY", setY_1)
+        .def("setY", setY_2)
+        .def("setY", setY_3)
         .def("getFirstSubTube", &Tube::getFirstSubTube, return_value_policy<manage_new_object>())
         .def("getSecondSubTube", &Tube::getSecondSubTube, return_value_policy<manage_new_object>())
         .def("getIntegral", &Tube::getIntegral)
-        //.def("intersect", &Tube::intersect)
-        //.def("getEnclosedBounds", &Tube::getEnclosedBounds)
+        .def("intersect", intersect_1)
+        .def("intersect", intersect_2)
+        .def("intersect", intersect_3)
+        .def("getEnclosedBounds", getEnclosedBounds_1)
+        .def("getEnclosedBounds", getEnclosedBounds_2)
         ;
 };
